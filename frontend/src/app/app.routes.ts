388b53f79df38b6f977595fd46bffd921b6c6f09
Sumit Shende <sumitshende535@gmail.com>
2025-05-28 00:14:22 +0530

    create success-page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { PageNotFoundComponent } from './features/errors/page-not-found/page-not-found.component';
 import { PatientFormComponent } from './features/patient-form/patient-form.component';
 import { FileInputComponent } from './features/file-input/file-input.component';
 import { AppointmentFormComponent } from './features/appointment-form/appointment-form.component';
+import { SuccessPageComponent } from './features/success-page/success-page.component';
 export const routes: Routes = [
   {
     path: '',
@@ -20,7 +22,11 @@ export const routes: Routes = [
   },
   {
     path: 'appointment-form',
-    component: AppointmentFormComponent
+    component: AppointmentFormComponent,
+  },
+  {
+    path: 'success-page',
+    component: SuccessPageComponent,
   },
   {
     path: '**',
